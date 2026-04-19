a70f0b8069bc678007b0f8c371acf41e7de40783
Akshat Dangwal <akshatdangwal07@gmail.com~>
2026-04-19 10:10:03 +0530

    fixing...

--- main.py
+++ main.py
@@ -235,7 +235,7 @@ async def ingest(request: Request):
     if slot == "land":
         print("📸 LAND SLOT - CAPTURING IMAGE")
 
-        image_data = fetch_image_from_camera(IP_CAMERA_URL)
+        image_data = fetch_image_from_camera()
 
         if image_data is not None:
             filename = os.path.join(IMAGE_DIR, f"image_{ts}_{int(time.time() * 1000)}.jpg")
